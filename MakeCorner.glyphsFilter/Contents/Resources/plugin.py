afff9e26d13f3bb945743984e3a43f26a76f90db
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-01-07 23:03:18 +0100

    Minor tweaks under the hood.

--- MakeCorner.glyphsFilter/Contents/Resources/plugin.py
+++ MakeCorner.glyphsFilter/Contents/Resources/plugin.py
@@ -17,7 +17,10 @@ from GlyphsApp.plugins import *
 class MakeCorner(FilterWithoutDialog):
 	
 	def settings(self):
-		self.menuName = Glyphs.localize({'en': u'Make Corner', 'de': u'Ecke herstellen'})
+		self.menuName = Glyphs.localize({
+			'en': u'Make Corner',
+			'de': u'Ecke herstellen'
+		})
 		self.keyboardShortcut = None # With Cmd+Shift
 	
 	def intersection( self, pointA, pointB, pointC, pointD ):
@@ -25,44 +28,39 @@ class MakeCorner(FilterWithoutDialog):
 		Returns an NSPoint of the intersection AB with CD.
 		Or False if there is no intersection
 		"""
+		x1, y1 = pointA.x, pointA.y
+		x2, y2 = pointB.x, pointB.y
+		x3, y3 = pointC.x, pointC.y
+		x4, y4 = pointD.x, pointD.y
+		
 		try:
-			x1, y1 = pointA.x, pointA.y
-			x2, y2 = pointB.x, pointB.y
-			x3, y3 = pointC.x, pointC.y
-			x4, y4 = pointD.x, pointD.y
+			slope12 = ( float(y2) - float(y1) ) / ( float(x2) - float(x1) )
+		except:
+			slope12 = None # vertical
 			
-			try:
-				slope12 = ( float(y2) - float(y1) ) / ( float(x2) - float(x1) )
-			except:
-				slope12 = None # vertical
-				
-			try:
-				slope34 = ( float(y4) - float(y3) ) / ( float(x4) - float(x3) )
-			except:
-				slope34 = None # vertical
-			
-			if slope12 == slope34:
-				# parallel, no intersection
-				return False
-			elif slope12 is None:
-				# first line is vertical
-				x = x1
-				y = slope34 * ( x - x3 ) + y3
-			elif slope34 is None:
-				# second line is vertical
-				x = x3
-				y = slope12 * ( x - x1 ) + y1
-			else:
-				# both lines have an angle
-				x = ( slope12 * x1 - y1 - slope34 * x3 + y3 ) / ( slope12 - slope34 )
-				y = slope12 * ( x - x1 ) + y1
-	
-			return NSPoint( x, y )
-			
-		except Exception as e:
-			self.logToConsole( "intersection: %s" % str(e) )
+		try:
+			slope34 = ( float(y4) - float(y3) ) / ( float(x4) - float(x3) )
+		except:
+			slope34 = None # vertical
+		
+		if slope12 == slope34:
+			# parallel, no intersection
 			return False
-	
+		elif slope12 is None:
+			# first line is vertical
+			x = x1
+			y = slope34 * ( x - x3 ) + y3
+		elif slope34 is None:
+			# second line is vertical
+			x = x3
+			y = slope12 * ( x - x1 ) + y1
+		else:
+			# both lines have an angle
+			x = ( slope12 * x1 - y1 - slope34 * x3 + y3 ) / ( slope12 - slope34 )
+			y = slope12 * ( x - x1 ) + y1
+
+		return NSPoint( x, y )
+
 	def filter(self, Layer, inEditView, customParameters):
 		selection = Layer.selection
 		
@@ -125,10 +123,9 @@ class MakeCorner(FilterWithoutDialog):
 			ghostPath.closed = True
 			ghostLayer.paths.append( ghostPath )
 		
-		Layer.setPaths_( None )
-		
-		for thisPath in ghostLayer.paths:
-			Layer.addPath_( thisPath )
+		Layer.paths = ghostLayer.paths
+		if selectionCounts:
+			Layer.selection = None
 	
 	def __file__(self):
 		"""Please leave this method unchanged"""
--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # MakeCorner.glyphsFilter
 
-This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert.
-It reverses the functionality of the Round Corner filter, and turns curves into corners again.
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert. It reverses the functionality of the *Round Corner* filter, and turns curve segments into corners again.
 
 ![Select an off-curve point and run the Make Corner filter.](MakeCorner.png "Make Corner Screenshot")
 
-After installation, it will add the menu item *Filter > Make Corner*.
-You can set a keyboard shortcut in System Preferences.
+After installation, it will add the menu item *Filter > Make Corner* (de: *Ecke herstellen*). You can set a keyboard shortcut in System Preferences.
 
 ### Installation
 
-1. Download the complete ZIP file and unpack it, or clone the repository.
-2. Double click the .glyphsFilter file. Confirm the dialog that appears in Glyphs.
-3. Restart Glyphs
+1. *Window > Plugin Manager.*
+2. Click the *Install* button next to the *Make Corner* entry.
+3. Restart Glyphs.
 
 ### Usage Instructions
 
@@ -24,18 +22,18 @@ You can set a keyboard shortcut in System Preferences.
 Although I am not sure if it makes sense to anyone, you can trigger the filter functionality via a custom parameter:
 
     Property: Filter
-    Value: GlyphsFilterMakeCorner;
+    Value: MakeCorner;
 
 At the end of the parameter value, you can hang `exclude:` or `include:`, followed by a comma-separated list of glyph names. This will apply the filter only to the included glyphs, or the glyphs not excluded, respectively.
 
 ### Requirements
 
-The plugin needs Glyphs 1.4.3 or higher, running on OS X 10.7 or later. I can only test it in current OS versions, and I assume it will not work in versions of Mac OS X older than 10.7.
+The plugin needs Glyphs 2.4 or higher. I assume it will not work in previous versions.
 
 ### License
 
-Copyright 2014 Rainer Erich Scheichelbauer (@mekkablue).
-Based on sample code by Georg Seifert (@schriftgestalt).
+Copyright 2014-2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
